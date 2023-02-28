b15696fbf69e4dbb599394c91e8029fb5e5c8452
Daniil Antoniuk <flyjeray.dev@gmail.com>
2023-02-28 18:01:01 +0300

    formatter setup

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,5 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 import { TRANSLATIONS } from './translations';
 
@@ -12,15 +12,13 @@ const resources = {
   },
 };
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: "en",
-    keySeparator: false,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
